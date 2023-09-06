dff9630b19000b991b52409b468fdc58ebf63397
Bhalodiya Vivek <102016678+bhalodiya-vivek@users.noreply.github.com>
2023-09-06 12:30:03 +0530

    Rename project.

--- ios/ChenoaPracticalTests/ChenoaPracticalTests.m
+++ ios/ChenoaPracticalTests/ChenoaPracticalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface demoTests : XCTestCase
+@interface ChenoaPracticalTests : XCTestCase
 
 @end
 
-@implementation demoTests
+@implementation ChenoaPracticalTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
